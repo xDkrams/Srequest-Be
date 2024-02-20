e352c822fa5851b3e3dc81297b687a83f55d3366
krams <mstaborda00@gmail.com>
2024-02-20 09:17:05 +0800

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// app.js
+
+const express = require("express");
+
+// Create Express app
+const app = express();
+app.use(express.json());
+
+// Dummy data
+const books = [
+  { id: 1, title: "Book 1", author: "Author 1" },
+  { id: 2, title: "Book 2", author: "Author 2" },
+  { id: 3, title: "Book 3", author: "Author 3" },
+];
+
+// Routes
+app.get("/books", (req, res) => {
+  res.json(books);
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const book = books.find((book) => book.id === id);
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({ message: "Book not found" });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
