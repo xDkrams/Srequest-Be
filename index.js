15f994c962b2be98a8e0fa3bfa8c81a26e69351b
krams <mstaborda00@gmail.com>
2024-02-20 09:52:08 +0800

    feat: minimal testing

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-// app.js
-
 const express = require("express");
 
-// Create Express app
 const app = express();
-app.use(express.json());
+const PORT = 8000;
 
-// Dummy data
-const books = [
-  { id: 1, title: "Book 1", author: "Author 1" },
-  { id: 2, title: "Book 2", author: "Author 2" },
-  { id: 3, title: "Book 3", author: "Author 3" },
-];
-
-// Routes
-app.get("/books", (req, res) => {
-  res.json(books);
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.get("/books/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const book = books.find((book) => book.id === id);
-  if (book) {
-    res.json(book);
-  } else {
-    res.status(404).json({ message: "Book not found" });
-  }
+app.get("/about", (req, res) => {
+  res.send("About route 🎉 ");
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`✅ Server is running on port ${PORT}`);
 });
